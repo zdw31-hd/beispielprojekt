0b3de7703a391f602e0d3b05800521aa2be0604e
zdw31-hd <145539001+zdw31-hd@users.noreply.github.com>
2023-11-30 09:17:50 +0000

    Temporäre Dateien gelöscht

--- tsdev/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Main {
-    static sayHello(n) {
-        for (let i = 0; i < n; i++)
-            console.log("World");
-    }
-}
-Main.sayHello(200);
-//# sourceMappingURL=index.js.map
