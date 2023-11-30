48535730891e815777c1c49da07db5c16c4e6a44
zdw31-hd <145539001+zdw31-hd@users.noreply.github.com>
2023-11-30 09:12:32 +0000

    first commit

---
+++ tsdev/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+class Main {
+    static sayHello(n) {
+        for (let i = 0; i < n; i++)
+            console.log("World");
+    }
+}
+Main.sayHello(200);
+//# sourceMappingURL=index.js.map
---
+++ tsdev/src/index.ts
@@ -0,0 +1,8 @@
+class Main {
+    static sayHello(n:number){
+        for (let i=0; i<n; i++)
+            console.log("World");
+    }
+}
+
+Main.sayHello(200);
